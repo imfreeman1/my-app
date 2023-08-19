74eff573d62d05d7e642dc91fc83038cafef6455
imfreeman1 <dkfrhkt@gmail.com>
2023-08-19 17:06:42 +0900

    아이콘 추가 및 수정함수 삭제함수 제작

--- renderer/components/TodoItem/index.tsx
+++ renderer/components/TodoItem/index.tsx
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Input from "../Input";
 import Button from "../Button";
 import { useRecoilState } from "recoil";
 import { todoListAtom } from "../../recoil/todo";
+import { AiFillEdit, AiFillDelete } from "react-icons/ai";
+
 import { defaultTodoType } from "../../recoil/todo/type";
 
 const TodoItem = ({ todo }) => {
@@ -20,6 +22,17 @@ const TodoItem = ({ todo }) => {
     setTodoList(newList);
   };
 
+  const modifyHandler = () => {
+    let modifyList: defaultTodoType[] = structuredClone(todoList);
+  };
+
+  const deleteHandler = (todoId) => {
+    let deleteList: defaultTodoType[] = structuredClone(todoList);
+    deleteList = deleteList.filter(({ id }) => id !== todoId);
+
+    setTodoList(deleteList);
+  };
+
   return (
     <li className="m-2">
       <div className="bg-baseWhite text-black flex gap-1 w-60 h-36 rounded-md p-2 items-center">
@@ -29,13 +42,19 @@ const TodoItem = ({ todo }) => {
           checked={todo.isCompleted}
           onChange={checkBoxHandler}
         />
-        <div className="grow flex flex-col h-full gap-1">
+        <div className="grow flex flex-col h-full gap-1 w-32 text-clip">
           <span className="font-semibold border-b-2">{todo.title}</span>
-          <span className="h-auto">{todo.content}</span>
+          <span className="h-auto w-full  whitespace-pre-line inline-block">
+            {todo.content}
+          </span>
         </div>
 
-        <Button className="h-fit">{"수정"}</Button>
-        <Button>{"아이콘"}</Button>
+        <Button className="h-fit p-1" onClick={modifyHandler}>
+          {<AiFillEdit size={24} />}
+        </Button>
+        <Button className="h-fit p-1" onClick={() => deleteHandler(todo.id)}>
+          {<AiFillDelete size={24} />}
+        </Button>
       </div>
     </li>
   );
