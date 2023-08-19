5a4eae646de636fc8cc955f6a2cd67eb51cebfbd
imfreeman1 <dkfrhkt@gmail.com>
2023-08-19 17:07:21 +0900

    사용하지 않는 atom 삭제

--- renderer/recoil/todo/atom.tsx
+++ renderer/recoil/todo/atom.tsx
@@ -1,23 +1,11 @@
 import { atom } from "recoil";
 import { defaultTodoType } from "./type";
 
-const defaultTodo: defaultTodoType = {
-  id: "",
-  title: "",
-  content: "",
-  isCompleted: false,
-};
-
-const defaultTodoList: defaultTodoType[] = [defaultTodo];
+const defaultTodoList: defaultTodoType[] = [];
 
 const todoListAtom = atom({
   key: "todoListAtom",
   default: defaultTodoList,
-});
-
-const todoAtom = atom({
-  key: "todoAtom",
-  default: defaultTodo,
 });
 
 export default todoListAtom;
