6116c24ed16c46ed91a7816f17c44d2af06b8565
imfreeman1 <dkfrhkt@gmail.com>
2023-08-21 18:10:06 +0900

    button type 필수로 변경

--- renderer/components/Button/type.tsx
+++ renderer/components/Button/type.tsx
@@ -4,6 +4,6 @@ export interface IButton {
   className: string;
   onClick: React.MouseEventHandler<HTMLButtonElement>;
   children: React.ReactNode | string;
-  type?: "button" | "submit" | "reset";
+  type: "button" | "submit" | "reset";
   disabled?: boolean;
 }
