9fba23fc16e8dee290f7719020f76b57f02efe0f
imfreeman1 <dkfrhkt@gmail.com>
2023-08-22 15:30:45 +0900

    todolist 페이지에 link 생성

--- renderer/pages/todolist.tsx
+++ renderer/pages/todolist.tsx
@@ -1,16 +1,12 @@
 import React from "react";
-import Link from "next/link";
 import TodoWrapper from "../components/TodoWrapper";
-
+import NavigationBar from "../components/NavigationBar";
+import { NAVIGATOR_CONSTANT } from "../constants/basicConstants/basicConstants";
 const TodoList = () => {
   return (
     <div className="grid grid-cols-12 grid-rows-8 h-screen">
-      {
-        // nav 위치
-      }
-      <Link href={"/home"}>
-        <a>back</a>
-      </Link>
+      <NavigationBar navList={NAVIGATOR_CONSTANT.list} isWhite={true} />
+
       <TodoWrapper />
     </div>
   );
