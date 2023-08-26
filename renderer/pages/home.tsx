caba5323afa1b86e45957611d45fbde2b0c91747
imfreeman1 <dkfrhkt@gmail.com>
2023-08-26 18:56:25 +0900

    home 문구 임시

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -5,7 +5,14 @@ import { NAVIGATOR_CONSTANT } from "../constants/basicConstants/basicConstants";
 // navBar에 css 변경 이벤트를 주려면 ul 태그와 li태그에 css 작업을 할 것.
 
 function Home() {
-  return <section className=" h-screen my-16 mx-1"></section>;
+  return (
+    <section className=" h-screen my-16 mx-1">
+      <div className="flex justify-center flex-col items-center h-3/5 text-8xl">
+        <span>Welcome!</span>
+        <span className="font-bold">It's My WebSite</span>
+      </div>
+    </section>
+  );
 }
 
 export default Home;
