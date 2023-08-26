d302b4a86aaa7aca9830ad7f88f535e814d8c4bd
imfreeman1 <dkfrhkt@gmail.com>
2023-08-26 17:17:22 +0900

    home 사용하지 않는 nav 삭제

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -5,11 +5,7 @@ import { NAVIGATOR_CONSTANT } from "../constants/basicConstants/basicConstants";
 // navBar에 css 변경 이벤트를 주려면 ul 태그와 li태그에 css 작업을 할 것.
 
 function Home() {
-  return (
-    <section className=" h-screen my-16 mx-1">
-      <NavigationBar navList={NAVIGATOR_CONSTANT.list} />
-    </section>
-  );
+  return <section className=" h-screen my-16 mx-1"></section>;
 }
 
 export default Home;
