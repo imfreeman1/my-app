8562d93c5bee0bfed81e0f4ce1ae6a759f3f02df
imfreeman1 <dkfrhkt@gmail.com>
2023-08-26 18:08:09 +0900

    게시판 레이아웃 나누기

--- renderer/pages/board.tsx
+++ renderer/pages/board.tsx
@@ -3,7 +3,10 @@ import React from "react";
 const board = () => {
   return (
     <>
-      <section>1</section>
+      <section className="h-screen grid grid-cols-3 gap-5">
+        <div>sidebar</div>
+        <div>Board</div>
+      </section>
     </>
   );
 };
