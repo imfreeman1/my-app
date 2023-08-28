2efb48aa851e5e9645fcae5a73f8e7b46451a515
imfreeman1 <dkfrhkt@gmail.com>
2023-08-28 18:15:06 +0900

    Component 분리 작업

--- renderer/components/BoardWrapper/index.tsx
+++ renderer/components/BoardWrapper/index.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import React from "react";
 import Title from "../Title";
 import SearchBar from "../SearchBar";
+import MessageBoard from "../MessageBoard";
 
 const BoardWrapper = () => {
   return (
@@ -10,22 +11,11 @@ const BoardWrapper = () => {
       <div className=" col-span-2 mt-20">
         <Title className={"text-4xl"}>게시판</Title>
         <SearchBar />
-        <div className="flex flex-col items-center">
-          <div className="w-full border-2 rounded-md p-4">
-            <ol className="gap-2">
-              <li>
-                <Link href={"/"}>boardList</Link>
-              </li>
-              <li>
-                <Link href={"/"}> 마찬가지</Link>{" "}
-              </li>
-            </ol>
-          </div>
-        </div>
+        <MessageBoard />
 
         <div className="flex justify-end gap-5 mr-3">
           <button>삭제</button>
-          <button>글쓰기</button>
+          <Link href={"/board/write"}>글쓰기</Link>
         </div>
       </div>
     </section>
