418d4716e6b9ea79e5927d656da7d76638cc7497
imfreeman1 <dkfrhkt@gmail.com>
2023-08-28 18:18:11 +0900

    searchBar 수정
     - 검색 Button Children을 Icon으로 변경
     - input css 수정

--- renderer/components/SearchBar/index.tsx
+++ renderer/components/SearchBar/index.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import Input from "../Input";
 import Button from "../Button";
+import { AiOutlineSearch } from "react-icons/ai";
 
 const SearchBar = () => {
   return (
-    <div>
-      <form>
-        <Input type="text" placeholder="검색" />
-        <Button className="btn-blue" type="button">
-          아이콘 쓸거임
+    <div className=" my-2">
+      <form className="flex justify-center gap-2">
+        {
+          // 여기에 option selector 넣을거
+        }
+        <Input
+          className="p-1 w-1/2 rounded-md"
+          type="text"
+          placeholder="검색"
+        />
+        <Button type="button" className="p-1">
+          <AiOutlineSearch size={30} />
         </Button>
       </form>
     </div>
