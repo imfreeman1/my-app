656dfab8d4d60c8b7d3469e0757080406e491791
imfreeman1 <dkfrhkt@gmail.com>
2023-08-30 19:34:49 +0900

    게시글 페이지 레이아웃 및 기능 구현

--- renderer/pages/board/read/[id].tsx
+++ renderer/pages/board/read/[id].tsx
@@ -1,13 +1,41 @@
 import { useRouter } from "next/router";
 import React from "react";
+import { useRecoilValue } from "recoil";
+import { boardListAtom } from "../../../recoil/board";
+import Button from "../../../components/Button";
 
 const Board = () => {
   const router = useRouter();
-  console.log("in id", router);
   const { id } = router.query;
+  const boardList = useRecoilValue(boardListAtom);
+  const onClick = () => {
+    router.back();
+  };
+
+  const findItem = structuredClone(boardList).filter((val) => id === val.id)[0];
   return (
     <section className="h-screen grid grid-cols-4 gap-5">
-      <div>{id}</div>
+      <div className="flex flex-col mt-20 col-start-2 col-span-2 w-full h-full">
+        <div className="flex justify-between p-3 items-center border-b-2 gap-2 border-gray-500">
+          <h2 className="text-2xl">제목 : {findItem.title}</h2>
+          <div>
+            <span className="text-gray-400 text-sm h-fit mr-2">
+              {findItem.time}
+            </span>
+            <span className="text-gray-400 text-sm h-fit">
+              조회 : {findItem.count}
+            </span>
+          </div>
+        </div>
+        <div className="p-5 min-h-1/2">
+          <span>{findItem.content}</span>
+        </div>
+        <div className="flex justify-end">
+          <Button onClick={onClick} type="button" className="btn-gray w-fit">
+            목록
+          </Button>
+        </div>
+      </div>
     </section>
   );
 };
