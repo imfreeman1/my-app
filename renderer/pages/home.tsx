ceb11402d703ded18b04ccce0c8e63225ba649da
imfreeman1 <dkfrhkt@gmail.com>
2023-08-31 18:58:22 +0900

    darkMode test

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,11 +1,23 @@
 import React from "react";
-import NavigationBar from "../components/NavigationBar";
-import { NAVIGATOR_CONSTANT } from "../constants/basicConstants/basicConstants";
+import { useTheme } from "next-themes";
 
 // navBar에 css 변경 이벤트를 주려면 ul 태그와 li태그에 css 작업을 할 것.
 
 function Home() {
-  return <section className=" h-screen my-16 mx-1"></section>;
+  const { theme, setTheme } = useTheme();
+
+  const onClick = () => {
+    let newTheme = theme === "dark" ? "light" : "dark";
+    console.log(newTheme);
+    setTheme(newTheme);
+  };
+  return (
+    <section className="h-screen my-16 mx-1">
+      <button type="button" onClick={onClick}>
+        변경
+      </button>
+    </section>
+  );
 }
 
 export default Home;
