da82d70e380e36e2482d6d8e26d1b30dd979e4a2
imfreeman1 <dkfrhkt@gmail.com>
2023-09-05 19:37:18 +0900

    theme 기능 제작 중
     - 현재 useTheme를 사용하여도 theme가 변경되지 않는 문제가 있음.
     - 라이브러리 공부를 더 하고 기능 구현 예정.

--- renderer/components/NavigationBar/index.tsx
+++ renderer/components/NavigationBar/index.tsx
@@ -1,17 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavItem from "../NavItem";
 import { NavigationBarType } from "./type";
+import { AiFillGithub, AiOutlineHome } from "react-icons/ai";
+import Link from "next/link";
+import Button from "../Button";
+import { useTheme } from "next-themes";
 
 const NavigationBar = ({ navList, isWhite }: NavigationBarType<string>) => {
   const NavItemClass = isWhite ? "nav-btn-white" : "nav-btn";
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+  console.log("tt", theme);
+  const onClick = () => {
+    let newTheme = theme === "dark" ? "light" : "dark";
+    console.log(newTheme);
+    setTheme(newTheme);
+  };
 
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex justify-between items-center px-6">
+      <Button className="nav-btn-white" type="button">
+        <Link href="/home">
+          <AiOutlineHome size={24} />
+        </Link>
+      </Button>
+
       <ul className="flex justify-center gap-6 h-16 w-fit items-center">
         {navList.map((nav, idx) => {
           return <NavItem key={idx} content={nav} className={NavItemClass} />;
         })}
       </ul>
+      <div className="flex gap-3 items-center">
+        <Button className="nav-btn-white p-3" type="button">
+          <Link href={"https://github.com/imfreeman1"} target="_blank">
+            <AiFillGithub size={24} />
+          </Link>
+        </Button>
+        <select
+          value={theme}
+          className="bg-gray-700 text-white p-1 rounded-md"
+          onChange={(e) => setTheme(e.target.value)}
+        >
+          <option value="system">System</option>
+          <option value="light">Light</option>
+          <option value="dark">Dark</option>
+        </select>
+        <button type="button" onClick={(e) => onClick(e)}>
+          확인용
+        </button>
+      </div>
     </div>
   );
 };
--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,23 +1,9 @@
 import React from "react";
-import { useTheme } from "next-themes";
 
 // navBar에 css 변경 이벤트를 주려면 ul 태그와 li태그에 css 작업을 할 것.
 
 function Home() {
-  const { theme, setTheme } = useTheme();
-
-  const onClick = () => {
-    let newTheme = theme === "dark" ? "light" : "dark";
-    console.log(newTheme);
-    setTheme(newTheme);
-  };
-  return (
-    <section className="h-screen my-16 mx-1">
-      <button type="button" onClick={onClick}>
-        변경
-      </button>
-    </section>
-  );
+  return <section className="h-screen my-16 mx-1"></section>;
 }
 
 export default Home;
