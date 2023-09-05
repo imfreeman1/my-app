94b7b676243742f5bb92527339f7c925a7e4f991
imfreeman1 <dkfrhkt@gmail.com>
2023-09-05 19:38:31 +0900

    Button의 onClick함수 옵셔널로 변경
     - 이걸 옵셔널로 바꿔도 될지는 조금 더 생각해보고 결정해야 할 것 같다.

--- renderer/components/Button/type.tsx
+++ renderer/components/Button/type.tsx
@@ -2,7 +2,7 @@ import { DetailedHTMLProps } from "react";
 
 export interface IButton {
   className?: string;
-  onClick: React.MouseEventHandler<HTMLButtonElement>;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
   children: React.ReactNode | string;
   type: "button" | "submit" | "reset";
   disabled?: boolean;
