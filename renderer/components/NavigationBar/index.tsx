4e0caf7e6c918dcf00e9fde95c54d4c31f7bd65a
imfreeman1 <dkfrhkt@gmail.com>
2023-09-07 19:41:18 +0900

    Theme 적용 완료 및 warning fix
     - Link 태그 내부에 component를 바로 넣게 될 경우 Warning이 발생하여 a태그로 한번 감싸줘서 해결

--- renderer/components/NavigationBar/index.tsx
+++ renderer/components/NavigationBar/index.tsx
@@ -1,29 +1,26 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import NavItem from "../NavItem";
 import { NavigationBarType } from "./type";
-import { AiFillGithub, AiOutlineHome } from "react-icons/ai";
+import { AiFillGithub, AiFillHome } from "react-icons/ai";
 import Link from "next/link";
 import Button from "../Button";
 import { useTheme } from "next-themes";
 
 const NavigationBar = ({ navList, isWhite }: NavigationBarType<string>) => {
   const NavItemClass = isWhite ? "nav-btn-white" : "nav-btn";
-  const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
-  console.log("tt", theme);
-  const onClick = () => {
-    let newTheme = theme === "dark" ? "light" : "dark";
-    console.log(newTheme);
-    setTheme(newTheme);
-  };
 
   return (
     <div className="flex justify-between items-center px-6">
-      <Button className="nav-btn-white" type="button">
-        <Link href="/home">
-          <AiOutlineHome size={24} />
-        </Link>
-      </Button>
+      <div className=" w-20">
+        <Button className="nav-btn-white" type="button">
+          <Link href="/home">
+            <a>
+              <AiFillHome size={24} />
+            </a>
+          </Link>
+        </Button>
+      </div>
 
       <ul className="flex justify-center gap-6 h-16 w-fit items-center">
         {navList.map((nav, idx) => {
@@ -33,7 +30,9 @@ const NavigationBar = ({ navList, isWhite }: NavigationBarType<string>) => {
       <div className="flex gap-3 items-center">
         <Button className="nav-btn-white p-3" type="button">
           <Link href={"https://github.com/imfreeman1"} target="_blank">
-            <AiFillGithub size={24} />
+            <a>
+              <AiFillGithub size={24} />
+            </a>
           </Link>
         </Button>
         <select
@@ -41,13 +40,9 @@ const NavigationBar = ({ navList, isWhite }: NavigationBarType<string>) => {
           className="bg-gray-700 text-white p-1 rounded-md"
           onChange={(e) => setTheme(e.target.value)}
         >
-          <option value="system">System</option>
           <option value="light">Light</option>
           <option value="dark">Dark</option>
         </select>
-        <button type="button" onClick={(e) => onClick(e)}>
-          확인용
-        </button>
       </div>
     </div>
   );
