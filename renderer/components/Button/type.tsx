81a6c16ef98611344f329d70b360165582b91ec1
imfreeman1 <dkfrhkt@gmail.com>
2023-09-07 19:43:32 +0900

    button의 children 수정
     - number type도 받을 수 있게 수정

--- renderer/components/Button/type.tsx
+++ renderer/components/Button/type.tsx
@@ -3,7 +3,7 @@ import { DetailedHTMLProps } from "react";
 export interface IButton {
   className?: string;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
-  children: React.ReactNode | string;
+  children: React.ReactNode | string | number;
   type: "button" | "submit" | "reset";
   disabled?: boolean;
 }
