d03ae57c9b1a0c02692ca4d7279b9793adc6951c
imfreeman1 <dkfrhkt@gmail.com>
2023-09-07 19:43:11 +0900

    MessageBoard 기능 구현
     - 일정 갯수가 넘어가면 다음페이지가 생성되도록 만듦.
     - 해당 페이지 번호를 누르면 범위에 속하는 item 나오도록 만듦.

--- renderer/components/MessageBoard/index.tsx
+++ renderer/components/MessageBoard/index.tsx
@@ -1,21 +1,53 @@
-import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useRecoilValue } from "recoil";
 import { boardListAtom } from "../../recoil/board";
 import BoardItem from "../BoardItem";
 import BoardColumn from "../BoardColumn";
+import Button from "../Button";
 
 const MessageBoard = () => {
   const boardList = useRecoilValue(boardListAtom);
+  const [number, setNumber] = useState<number>(1);
+  const [numberList, setNumberList] = useState<number[]>([]);
+  const showBoardList = boardList.slice(6 * (number - 1), 6 * number);
+  useEffect(() => {
+    const makeNumberList = () => {
+      let count = Math.ceil(boardList.length / 6) || 1;
+      setNumberList(
+        Array(count)
+          .fill("")
+          .map((val, idx) => idx + 1)
+      );
+    };
+    makeNumberList();
+  }, [boardList]);
+
   return (
     <div className="flex flex-col items-center my-4">
-      <div className="w-full border-2 rounded-md p-4 relative">
+      <div className="w-full border-2 border-gray-400 rounded-md p-4 relative">
         <BoardColumn />
-        <ol className="gap-2 mt-7">
-          {boardList.map((bulletin) => {
-            return <BoardItem key={bulletin.id} bulletin={bulletin} />;
+        <div className="mt-7 h-80">
+          <ol>
+            {showBoardList.map((bulletin) => {
+              return <BoardItem key={bulletin.id} bulletin={bulletin} />;
+            })}
+          </ol>
+        </div>
+        <div className="flex justify-center gap-2 items-end">
+          {numberList.map((val) => {
+            return (
+              <Button
+                className={`${
+                  number === val ? "text-xl font-bold" : "text-base"
+                }`}
+                type="button"
+                onClick={() => setNumber(val)}
+              >
+                {val}
+              </Button>
+            );
           })}
-        </ol>
+        </div>
       </div>
     </div>
   );
