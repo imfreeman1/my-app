ac2b0f407541f863575afb8d57426a36fc496f27
imfreeman1 <dkfrhkt@gmail.com>
2023-09-11 19:46:26 +0900

    searchOptionSubmit 함수 생성

--- renderer/components/SearchBar/index.tsx
+++ renderer/components/SearchBar/index.tsx
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "../Input";
 import Button from "../Button";
 import { AiOutlineSearch } from "react-icons/ai";
+import { SearchBarType } from "./type";
 
-const SearchBar = () => {
+const SearchBar: React.FC<SearchBarType> = ({ setSelectorOption }) => {
+  const [option, setOption] = useState<string>("title");
+  const [keyword, setKeyword] = useState<string>("");
+
+  const searchOptionSubmit = (e) => {
+    e.preventDefault();
+    setSelectorOption({ option, keyword });
+  };
   return (
     <div className=" my-2">
-      <form className="flex justify-center gap-2">
+      <form
+        className="flex justify-center gap-2"
+        onSubmit={(e) => searchOptionSubmit(e)}
+      >
         <div className="w-fit">
-          <select className="rounded-md p-2 h-full mr-1">
-            <option>제목</option>
+          <select
+            className="rounded-md p-2 h-full mr-1"
+            onChange={({ target }) => setOption(target.value)}
+          >
+            <option value="title">제목</option>
+            <option value="content">글 내용</option>
           </select>
-          <Input className="p-2 rounded-md" type="text" placeholder="검색" />
+          <Input
+            className="p-2 rounded-md"
+            type="text"
+            value={keyword}
+            onChange={({ target }) => setKeyword(target.value)}
+            placeholder="검색"
+          />
         </div>
 
-        <Button type="button" className="p-1">
+        <Button type="submit" className="p-1">
           <AiOutlineSearch size={30} />
         </Button>
       </form>
