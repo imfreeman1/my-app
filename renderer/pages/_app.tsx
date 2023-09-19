a3a87da75b10cf94c77cead8b28a2bc9fb2e4bf9
imfreeman1 <dkfrhkt@gmail.com>
2023-09-19 19:05:35 +0900

    QueryClientProvider  추가

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -4,15 +4,20 @@ import { RecoilRoot } from "recoil";
 import "../styles/globals.css";
 import Layout from "../components/Layout";
 import { ThemeProvider } from "next-themes";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
-      <RecoilRoot>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </RecoilRoot>
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
