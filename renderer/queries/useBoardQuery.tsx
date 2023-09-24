3b9483dd55761c311e95ca91fc52393ada1d69a2
imfreeman1 <dkfrhkt@gmail.com>
2023-09-24 15:35:07 +0900

    useBoardQuery 매개변수 타입 지정 및 collection board로 변경

--- renderer/queries/useBoardQuery.tsx
+++ renderer/queries/useBoardQuery.tsx
@@ -5,14 +5,14 @@ import { BulletinType } from "../recoil/board/type";
 
 const getData = async () => {
   const boardResArray: BulletinType[] = [];
-  const q = collection(db, "portpolioDB");
+  const q = collection(db, "board");
   const res = await getDocs(q);
   res.forEach((resData) => boardResArray.push(resData.data() as BulletinType));
   boardResArray.sort((a, b) => b.index - a.index);
   return boardResArray;
 };
 
-const useBoardQuery = (callBack) => {
+const useBoardQuery = (callBack: Function) => {
   useQuery(["boardList"], getData, {
     onSuccess: (data) => {
       callBack(data);
