32bd293dba625097cf40bb9b0136ba66c87125db
imfreeman1 <dkfrhkt@gmail.com>
2023-10-06 18:04:49 +0900

    BoardItem 조회수 증가 기능 수정 및 css 수정
     - 기존의 State상에서 변경하던 것을 통신으로 해결 할 수 있게 변경

--- renderer/components/BoardItem/index.tsx
+++ renderer/components/BoardItem/index.tsx
@@ -1,38 +1,31 @@
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import { BoardItemType } from "./type";
-import { useRecoilState } from "recoil";
-import { boardListAtom } from "../../recoil/board";
-import { BulletinType } from "../../recoil/board/type";
 import Button from "../Button";
 import dateSplit from "../../utils/dateSplit";
 import dateStringMaker from "../../utils/dateUtils";
 import timeSplit from "../../utils/timeSplit";
 import { doc, increment, updateDoc } from "firebase/firestore";
 import { db } from "../../firebase";
+import { useRouter } from "next/router";
 
 const BoardItem: React.FC<BoardItemType> = ({ bulletin }) => {
-  const [boardList, setBoardList] = useRecoilState(boardListAtom);
-  // onClick를 통해 통신해서 boardItem의 count를 올릴 필요가 있음. firebase의 query 내부에 Where을 사용해야할 듯.
+  const router = useRouter();
   const onClick = async (id: string) => {
     const countRef = doc(db, "board", bulletin.id);
     await updateDoc(countRef, {
       count: increment(1),
     });
-    // let newBoardList: BulletinType[] = structuredClone(boardList);
-    // newBoardList = newBoardList.map((boardItem) => {
-    //   if (id === boardItem.id) {
-    //     boardItem.count += 1;
-    //     return boardItem;
-    //   }
-    //   return boardItem;
-    // });
-    // setBoardList(newBoardList);
   };
+
+  useEffect(() => {
+    router.prefetch(`/board/${bulletin.id}`);
+  }, [router]);
+
   return (
     <li>
       <div className="flex justify-between gap-4 items-center">
-        <span className="text-gray-700 dark:text-gray-200 text-sm h-fit pb-2">
+        <span className="text-gray-700 dark:text-gray-200 text-xs h-fit pb-2 w-8 text-center">
           {bulletin.index}
         </span>
         <div className="grow border-b-1 border-gray-200 dark:border-gray-100 m-2 pb-2 font-bold">
@@ -43,12 +36,12 @@ const BoardItem: React.FC<BoardItemType> = ({ bulletin }) => {
           </Button>
         </div>
         <div className="flex gap-8">
-          <span className="text-gray-700 dark:text-gray-200 text-sm h-fit pb-2 w-10 text-center">
+          <span className="text-gray-700 dark:text-gray-200 text-xs h-fit pb-2 text-center w-10">
             {dateStringMaker() === bulletin.date
               ? timeSplit(bulletin.time)
               : dateSplit(bulletin.date)}
           </span>
-          <span className="text-gray-700 dark:text-gray-200 text-sm h-fit pb-2">
+          <span className="text-gray-700 dark:text-gray-200 text-xs h-fit pb-2 w-10 text-center">
             {bulletin.count}
           </span>
         </div>
