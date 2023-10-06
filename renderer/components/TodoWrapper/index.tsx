c3f8253c167c6cec6ef0674eec9d3ccf7be5ebef
imfreeman1 <dkfrhkt@gmail.com>
2023-10-06 18:36:52 +0900

    TodoWrapper 수정
     - Todos Get hook 추가.
     - constants 분리

--- renderer/components/TodoWrapper/index.tsx
+++ renderer/components/TodoWrapper/index.tsx
@@ -1,43 +1,27 @@
 import React, { Suspense } from "react";
 import Card from "../Card";
 import Title from "../Title";
-import AntForm from "../AntForm";
-import TodoContainer from "../TodoContainer";
+import { TODO_CONSTANTS } from "../../constants/todoConstants/todoConstants";
+import { useRecoilState } from "recoil";
+import { todoListAtom } from "../../recoil/todo";
+import useGetTodos from "../../hook/useGetTodos";
 
 const TodoWrapper = () => {
-  const cardList = [
-    {
-      className:
-        "default-card h-fit bg-white shadow-xl border-1 border-gray-300",
-      title: "할일",
-      titleClass: "text-black",
-      childTag: <AntForm />,
-    },
-    {
-      className:
-        "default-card h-full w-72 bg-white shadow-xl border-1 border-gray-300",
-      title: "진행 중",
-      titleClass: "text-black text-2xl py-1 px-3",
-      childTag: <TodoContainer completed={false} />,
-    },
-    {
-      className:
-        "default-card h-full w-72 bg-white shadow-xl border-1 border-gray-300",
-      title: "완료",
-      titleClass: "text-black text-2xl py-1 px-3",
-      childTag: <TodoContainer completed={true} />,
-    },
-  ];
+  const [todoList, setTodoList] = useRecoilState(todoListAtom);
+  useGetTodos(todoList, setTodoList);
+
   return (
     <section className="col-start-2 col-end-12 row-start-2 row-span-6 h-auto flex justify-evenly">
-      {cardList.map(({ className, title, titleClass, childTag }, idx) => {
-        return (
-          <Card key={idx} className={className}>
-            <Title className={titleClass}>{title}</Title>
-            <Suspense fallback=<div></div>>{childTag}</Suspense>
-          </Card>
-        );
-      })}
+      {TODO_CONSTANTS.CARD_LIST.map(
+        ({ className, title, titleClass, childTag }, idx) => {
+          return (
+            <Card key={idx} className={className}>
+              <Title className={titleClass}>{title}</Title>
+              <Suspense fallback=<div>대기</div>>{childTag}</Suspense>
+            </Card>
+          );
+        }
+      )}
     </section>
   );
 };
---
+++ renderer/hook/useGetTodos.tsx
@@ -0,0 +1,18 @@
+import { collection, getDocs } from "firebase/firestore";
+import { useEffect } from "react";
+import { db } from "../firebase";
+
+const getTodos = async (callBack: Function) => {
+  const newArray = [];
+  const res = await getDocs(collection(db, "todos"));
+  res.forEach((todo) => newArray.push(todo.data()));
+  callBack(newArray);
+};
+
+const useGetTodos = (todoList: Object, callBack: Function) => {
+  useEffect(() => {
+    getTodos(callBack);
+  }, [todoList]);
+};
+
+export default useGetTodos;
