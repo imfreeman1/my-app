9d307748efa458c0619d6de1825aba7787a03d88
imfreeman1 <dkfrhkt@gmail.com>
2023-10-23 18:55:29 +0900

    default-card css를 Card Component로 이관

--- renderer/components/Card/index.tsx
+++ renderer/components/Card/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { CardType } from "./type";
 
 const Card = ({ className, children }: CardType) => {
-  return <div className={className}>{children}</div>;
+  return <div className={`default-card ${className}`}>{children}</div>;
 };
 
 export default Card;
--- renderer/constants/todoConstants/todoConstants.tsx
+++ renderer/constants/todoConstants/todoConstants.tsx
@@ -4,22 +4,19 @@ import TodoContainer from "../../components/TodoContainer";
 const TODO_CONSTANTS = {
   CARD_LIST: [
     {
-      className:
-        "default-card h-fit bg-white shadow-xl border-1 border-gray-300",
+      className: "h-fit bg-white shadow-xl border-1 border-gray-300",
       title: "할일",
       titleClass: "text-black",
       childTag: <AntForm />,
     },
     {
-      className:
-        "default-card h-full w-72 bg-white shadow-xl border-1 border-gray-300",
+      className: "h-full w-72 bg-white shadow-xl border-1 border-gray-300",
       title: "진행 중",
       titleClass: "text-black text-2xl py-1 px-3",
       childTag: <TodoContainer completed={false} />,
     },
     {
-      className:
-        "default-card h-full w-72 bg-white shadow-xl border-1 border-gray-300",
+      className: "h-full w-72 bg-white shadow-xl border-1 border-gray-300",
       title: "완료",
       titleClass: "text-black text-2xl py-1 px-3",
       childTag: <TodoContainer completed={true} />,
