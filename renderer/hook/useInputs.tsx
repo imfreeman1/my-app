906a7ff41ec0200abfc10202b28fe8ee26f9023c
imfreeman1 <dkfrhkt@gmail.com>
2023-11-09 17:36:54 +0900

    console.log 제거

--- renderer/hook/useInputs.tsx
+++ renderer/hook/useInputs.tsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 
 const useInputs = <T extends {}>(inputType: T): [T, Function, Function] => {
   const [inputs, setInputs] = useState(inputType);
-  console.log(inputs);
   const onChange = ({ target }) => {
     const { name, value } = target;
     setInputs((state) => ({ ...state, [name]: value }));
