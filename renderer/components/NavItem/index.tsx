0580ac1b573eade2b9ac0117ca6db37988dba220
imfreeman1 <dkfrhkt@gmail.com>
2023-12-18 19:39:58 +0900

    의미없는 a태그 span태그로 변경

--- renderer/components/NavItem/index.tsx
+++ renderer/components/NavItem/index.tsx
@@ -1,12 +1,12 @@
-import Link from "next/link";
-import React from "react";
-import { NavItemType } from "./type";
+import Link from 'next/link';
+import React from 'react';
+import { NavItemType } from './type';
 
 function NavItem({ content }: NavItemType) {
   return (
     <li className="nav-btn dark:text-black">
       <Link href={`/${content.toLowerCase()}`}>
-        <a>{content}</a>
+        <span>{content}</span>
       </Link>
     </li>
   );
