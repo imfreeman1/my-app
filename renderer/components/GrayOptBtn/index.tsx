879de1ad9365c362a60dda2d4afeb3e5cc9639e6
imfreeman1 <dkfrhkt@gmail.com>
2023-12-18 19:40:15 +0900

    li 태그 key값 변경

--- renderer/components/GrayOptBtn/index.tsx
+++ renderer/components/GrayOptBtn/index.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { useRecoilState, useSetRecoilState } from "recoil";
-import Button from "../Button";
-import { calAtom, calStringAtom } from "../../recoil/calculator";
-import { GRAY_BTN_METHOD } from "../../constants/btnConstants/btnConstants";
+import React from 'react';
+import { useRecoilState, useSetRecoilState } from 'recoil';
+import Button from '../Button';
+import { calAtom, calStringAtom } from '../../recoil/calculator';
+import { GRAY_BTN_METHOD } from '../../constants/btnConstants/btnConstants';
 
 // 나중엔 배열 풀자.
 
@@ -13,10 +13,10 @@ function GrayOptBtn() {
 
   return (
     <ul className="grid grid-cols-3 gap-1">
-      {Object.entries(GRAY_BTN_METHOD).map((entry, idx) => {
+      {Object.entries(GRAY_BTN_METHOD).map((entry) => {
         const [content, method] = entry;
         return (
-          <li key={idx}>
+          <li key={content}>
             <Button
               type="button"
               className="btn-gray"
